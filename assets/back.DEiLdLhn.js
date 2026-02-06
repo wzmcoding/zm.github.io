f23774116036bb2d8639b63bda646cd6aee04a0f
wzmcoding <wzmcoding@users.noreply.github.com>
2026-02-06 03:51:44 +0000

    deploy: 3b4f75221de986de9ee20274bd45a9e4b9eb83e2

---
+++ assets/back.DEiLdLhn.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.TsY6TDxB.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
